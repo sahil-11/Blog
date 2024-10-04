5582330be399a3e8e7391e86f29c77ab0bd424ce
Sahil Verma <sv81639@gmail.com>
2024-10-04 16:52:14 +0530

    Connect to the DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+dotenv.config();
+
+console.log(process.env.DATABASE);
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
