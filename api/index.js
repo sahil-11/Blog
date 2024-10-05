f4c2c6ae6a1f02900b5cdfcfd2ac843cc23ab85f
Sahil Verma <sv81639@gmail.com>
2024-10-05 16:41:12 +0530

    Created Signup API route along with error handler

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import morgan from "morgan";
 import bodyParser from "body-parser";
 import cors from "cors";
 import userRoutes from "./routes/user.route.js";
-
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -18,9 +18,21 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
