323b24336bcb3a3a650b9f0967c8dfdd4466c783
Sahil Verma <sv81639@gmail.com>
2024-10-05 15:56:29 +0530

    Created test api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import cors from "cors";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -20,3 +22,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes);
